3bb1fcc5b3ec3d5d900568dde56b15a1d7fad1b0
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-03-16 04:46:59 +0100

    implement and spec out XML feed parsing

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,16 +1,26 @@
 require 'cgi'
+require 'net/http'
+require 'rexml/document'
+require 'date'
 
 module Contacts
+  class FetchingError < RuntimeError
+    attr_reader :response
+    
+    def initialize(response)
+      @response = response
+      super "expected HTTPSuccess, got #{response.class} (#{response.code} #{response.message})"
+    end
+  end
+  
   # AuthSub proxy authentication is used by web applications that need to
   # authenticate their users to Google Accounts.
   # 
   # http://code.google.com/apis/contacts/developers_guide_protocol.html#auth_sub
-  module Gmail
-    AuthSubURL = 'https://www.google.com/accounts/AuthSubRequest'
-    # next / scope / secure / session
-    AuthScope = 'http://www.google.com/m8/feeds/'
-    FetchURL = 'http://www.google.com/m8/feeds/contacts/%s/base'
-    # max-results / start-index / updated-min / orderby lastmodified / sortorder
+  class Gmail
+    DOMAIN     = 'www.google.com'
+    AuthSubURL = "https://#{DOMAIN}/accounts/AuthSubRequest"
+    AuthScope  = "http://#{DOMAIN}/m8/feeds/"
 
     def self.authentication_url(target, options = {})
       params = { :next => target,
@@ -26,10 +36,105 @@ module Contacts
           else pair.last
           end
         
-        url << "#{pair.first}=#{CGI::escape(value.to_s)}"
+        url << "#{pair.first}=#{CGI.escape(value.to_s)}"
       end.join('&')
 
       AuthSubURL + '?' + query
+    end
+
+    attr_reader :uri
+    
+    def initialize(email, token, options = {})
+      @token = token.to_s
+      @email = email.to_s
+      @headers = {
+        'Authorization' => %(AuthSub token="#{@token}"),
+        'Accept-Encoding' => 'gzip'
+      }
+      @uri = URI.parse AuthScope + "contacts/#{CGI.escape(@email)}/base"
+      
+      @params = { :limit => 200 }.merge(options)
+    end
+
+    def query_string
+      @params.inject [] do |url, pair|
+        value = pair.last
+        
+        # max-results / start-index / updated-min / orderby lastmodified / sortorder
+        key = case pair.first
+          when :limit
+            'max-results'
+          when :offset
+            value = value.to_i + 1
+            'start-index'
+          when :order
+            url << 'sortorder=descending' unless @params.key? :descending
+            'orderby'
+          when :descending
+            value = value ? 'descending' : 'ascending'
+            'sortorder'
+          when :updated_after
+            value = value.strftime("%Y-%m-%dT%H:%M:%S%Z") if value.respond_to? :strftime
+            'updated-min'
+          else pair.first
+          end
+        
+        url << "#{key}=#{CGI.escape(value.to_s)}"
+      end.join('&')
+    end
+
+    def get
+      response = Net::HTTP.start(uri.host) do |google|
+        google.get(uri.path + '?' + query_string, @headers)
+      end
+
+      unless response.is_a? Net::HTTPSuccess
+        raise FetchingError.new(response)
+      end
+
+      response
+    end
+
+    def response_body
+      @response = get unless @response
+      
+      unless @response['Content-Encoding'] == 'gzip'
+        @response.body
+      else
+        require 'zlib'
+        require 'stringio'
+
+        gzipped = StringIO.new(@response.body)
+        Zlib::GzipReader.new(gzipped).read
+      end
+    end
+
+    def parse
+      @doc = REXML::Document.new response_body
+      @updated_string = REXML::XPath.first(@doc, '/feed/updated').text
+    end
+
+    def updated_at
+      DateTime.parse @updated_string if @updated_string
+    end
+
+    def updated_at_string
+      @updated_string
+    end
+
+    def contacts
+      parse
+      all = []
+      REXML::XPath.each(@doc, '/feed/entry') do |entry|
+        title_node = entry.get_elements('title').first
+        email_nodes = entry.get_elements('gd:email')
+        if title_node or !email_nodes.zero?
+          person = [title_node ? title_node.text : nil]
+          person.concat email_nodes.map {|e| e.attribute(:address).to_s }
+          all << person
+        end
+      end
+      all
     end
   end
 end
---
+++ spec/gmail/fetching_spec.rb
@@ -0,0 +1,77 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'contacts'
+require 'zlib'
+require 'stringio'
+
+describe Contacts::Gmail do
+  it 'is ready to query contacts from a specific account' do
+    create.uri.path.should include('/example%40gmail.com/')
+  end
+
+  it 'fetches contacts feed via HTTP GET' do
+    gmail = create
+    gmail.expects(:query_string).returns('a=b')
+    connection = mock('HTTP connection')
+    response = stub('HTTP response', :is_a? => true)
+    Net::HTTP.expects(:start).with('www.google.com').yields(connection).returns(response)
+    connection.expects(:get).with('/m8/feeds/contacts/example%40gmail.com/base?a=b', {
+        'Authorization' => %(AuthSub token="dummytoken"),
+        'Accept-Encoding' => 'gzip'
+      })
+
+    gmail.get
+  end
+
+  it 'handles gzipped response' do
+    gmail = create
+    response = mock('HTTP response')
+    gmail.expects(:get).returns(response)
+
+    gzipped = StringIO.new
+    gzwriter = Zlib::GzipWriter.new gzipped
+    gzwriter.write(('a'..'z').to_a.join)
+    gzwriter.close
+
+    response.expects(:'[]').with('Content-Encoding').returns('gzip')
+    response.expects(:body).returns gzipped.string
+
+    gmail.response_body.should == 'abcdefghijklmnopqrstuvwxyz'
+  end
+
+  it 'raises a FetchingError when something goes awry' do
+    gmail = create
+    response = mock('HTTP response', :code => 666, :class => Net::HTTPBadRequest, :message => 'oh my')
+    Net::HTTP.expects(:start).returns(response)
+
+    lambda {
+      gmail.get
+    }.should raise_error(Contacts::FetchingError)
+  end
+
+  it 'parses the resulting feed into name/email pairs' do
+    gmail = create
+    gmail.expects(:response_body).returns(sample_xml(:single))
+
+    gmail.contacts.should == [['Fitzgerald', 'fubar@gmail.com']]
+  end
+
+  it 'makes modification time available after parsing' do
+    gmail = create
+    gmail.updated_at.should be_nil
+    gmail.expects(:response_body).returns(sample_xml(:single))
+
+    gmail.contacts
+    u = gmail.updated_at
+    u.year.should == 2008
+    u.day.should == 5
+    gmail.updated_at_string.should == '2008-03-05T12:36:38.836Z'
+  end
+
+  def create(options = {})
+    Contacts::Gmail.new('example@gmail.com', 'dummytoken', options)
+  end
+
+  def sample_xml(name)
+    File.read File.dirname(__FILE__) + "/../feeds/#{name}.xml"
+  end
+end
